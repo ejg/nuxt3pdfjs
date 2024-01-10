8b9064263dbcdc5f55c23de9e6a7d32e720c3656
ejg <egillcoat@gmail.com>
2024-01-09 18:15:33 -0600

    pdfjs-express-viewer

---
+++ components/WebViewer.vue
@@ -0,0 +1,31 @@
+<template>
+  <div ref="viewer"></div>
+</template>
+
+<script>
+export default {
+  name: 'WebViewer',
+  mounted: function () {
+    import('@pdftron/pdfjs-express-viewer').then(() => {
+      WebViewer(
+        {
+          path: 'lib/',
+          initialDoc: `files/demo.pdf`,
+          licenseKey: 'VMeLR5MsW5lX3X9YfqQF',
+        },
+        this.$refs.viewer
+      ).then((instance) => {
+        // call apis here
+      });
+    });
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+div {
+  width: 100%;
+  height: 100vh;
+}
+</style>
